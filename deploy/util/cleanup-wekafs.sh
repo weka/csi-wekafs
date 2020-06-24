f1b3cd78db51cd929a35ba3ddb9779efe0735196
Sergey Berezansky <sergey@weka.io>
2020-06-24 10:16:09 +0300

    Removed old named enities cleanup

--- deploy/util/cleanup-wekafs.sh
+++ deploy/util/cleanup-wekafs.sh
@@ -17,25 +17,14 @@ echo "removing wekafs plugin daemonset"
 kubectl -n csi-wekafsplugin delete daemonsets.apps csi-wekafsplugin
 
 echo "removing wekafs plugin roles and permissions"
-# TODO: remove the old names
-kubectl -n csi-wekafsplugin delete clusterrolebindings.rbac.authorization.k8s.io csi-wekafsplugin-cluster-role-binding csi-attacher-role csi-provisioner-role csi-wekafs-attacher-role csi-wekafs-provisioner-role
+kubectl -n csi-wekafsplugin delete clusterrolebindings.rbac.authorization.k8s.io csi-wekafsplugin-cluster-role-binding
+kubectl -n csi-wekafsplugin delete clusterroles.rbac.authorization.k8s.io csi-wekafsplugin-cluster-role
+kubectl -n csi-wekafsplugin delete rolebindings.rbac.authorization.k8s.io csi-wekafsplugin-role-binding
+kubectl -n csi-wekafsplugin delete roles.rbac.authorization.k8s.io csi-wekafsplugin-role
+kubectl -n csi-wekafsplugin delete serviceaccounts csi-wekafsplugin
 
-# TODO: remove the old names
-kubectl -n csi-wekafsplugin delete clusterroles.rbac.authorization.k8s.io csi-wekafsplugin-cluster-role csi-wekafs-attacher-runner csi-wekafs-provisioner-runner
-
-# TODO: remove the old names
-kubectl -n csi-wekafsplugin delete rolebindings.rbac.authorization.k8s.io csi-wekafsplugin-role-binding csi-wekafs-attacher-role-cfg csi-wekafs-provisioner-role-cfg
-
-# TODO: remove the old names
-kubectl -n csi-wekafsplugin delete roles.rbac.authorization.k8s.io csi-wekafsplugin-role csi-wekafs-attacher-cfg csi-wekafs-provisioner-cfg
-
-# TODO: remove the old names
-kubectl -n csi-wekafsplugin delete serviceaccounts csi-wekafsplugin csi-wekafs-attacher csi-wekafs-provisioner
-
-
-# namespace
 echo "removing  wekafsplugin namespace"
-kubectl get namespace "$WEKAFS_NAMESPACE" &>/dev/null && kubectl delete namespace "$WEKAFS_NAMESPACE"
+kubectl delete namespace "$WEKAFS_NAMESPACE"
 
 
 echo $(date +%H:%M:%S) "cleanup completed successfully"
