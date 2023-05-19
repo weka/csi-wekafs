7a82c07a315fabd4e29c0526e8f8353bf296b389
Oleksandr Holubenko <oleksandr.holubenko@weka.io>
2023-05-19 13:34:14 +0300

    test rotation

--- pkg/wekafs/apiclient/apiclient.go
+++ pkg/wekafs/apiclient/apiclient.go
@@ -385,6 +385,7 @@ func (a *ApiClient) Request(ctx context.Context, Method string, Path string, Pay
 		log.Ctx(ctx).Error().Err(err).Msg("Failed to re-authenticate on repeating request")
 		return err
 	}
+	a.rotateEndpoint(ctx)
 	err := a.request(ctx, Method, Path, Payload, Query, Response)
 	if err != nil {
 		return err
