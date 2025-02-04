be13e783f99b804413e49e5d8097c2fc0db8fd6d
Sergey Berezansky <sergey@weka.io>
2025-02-04 23:16:45 +0200

    fix(CSI-315): pass encryption params to filesystem creation

--- pkg/wekafs/apiclient/encryption.go
+++ pkg/wekafs/apiclient/encryption.go
@@ -6,3 +6,12 @@ func (a *ApiClient) IsEncryptionEnabled() bool {
 	}
 	return true // TODO: implement the rest of the function to actually fetch the data
 }
+
+type EncryptionParams struct {
+	Encrypted             bool
+	AllowNoKms            bool
+	KmsVaultKeyIdentifier string
+	KmsVaultNamespace     string
+	KmsVaultRoleId        string
+	KmsVaultSecretId      string
+}
