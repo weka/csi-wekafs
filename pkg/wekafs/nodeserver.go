4c051220b7ab695d00bfef32ceb4f50f06bb44d0
Sergey Berezansky <sergey@weka.io>
2025-02-18 18:57:16 +0200

    feat(CSI-334): allow using multiple mounters in parallel on same server
    For example to be able to connect to different clusters by different protocols

--- pkg/wekafs/nodeserver.go
+++ pkg/wekafs/nodeserver.go
@@ -55,7 +55,7 @@ type NodeServer struct {
 	caps              []*csi.NodeServiceCapability
 	nodeID            string
 	maxVolumesPerNode int64
-	mounter           AnyMounter
+	mounters          *MounterGroup
 	api               *ApiStore
 	config            *DriverConfig
 	semaphores        map[string]*semaphore.Weighted
@@ -87,8 +87,12 @@ func (ns *NodeServer) getApiStore() *ApiStore {
 	return ns.api
 }
 
-func (ns *NodeServer) getMounter() AnyMounter {
-	return ns.mounter
+func (ns *NodeServer) getMounter(ctx context.Context) AnyMounter {
+	return ns.mounters.GetPreferredMounter(ctx)
+}
+
+func (ns *NodeServer) getMounterByTransport(ctx context.Context, transport DataTransport) AnyMounter {
+	return ns.mounters.GetMounterByTransport(ctx, transport)
 }
 
 //goland:noinspection GoUnusedParameter
@@ -194,7 +198,7 @@ func getVolumeStats(volumePath string) (volumeStats *VolumeStats, err error) {
 	return &VolumeStats{capacityBytes, usedBytes, availableBytes, inodes, inodesUsed, inodesFree}, nil
 }
 
-func NewNodeServer(nodeId string, maxVolumesPerNode int64, api *ApiStore, mounter AnyMounter, config *DriverConfig) *NodeServer {
+func NewNodeServer(nodeId string, maxVolumesPerNode int64, api *ApiStore, mounters *MounterGroup, config *DriverConfig) *NodeServer {
 	//goland:noinspection GoBoolExpressions
 	return &NodeServer{
 		caps: getNodeServiceCapabilities(
@@ -206,7 +210,7 @@ func NewNodeServer(nodeId string, maxVolumesPerNode int64, api *ApiStore, mounte
 		),
 		nodeID:            nodeId,
 		maxVolumesPerNode: maxVolumesPerNode,
-		mounter:           mounter,
+		mounters:          mounters,
 		api:               api,
 		config:            config,
 		semaphores:        make(map[string]*semaphore.Weighted),
@@ -537,7 +541,7 @@ func (ns *NodeServer) NodeGetInfo(ctx context.Context, req *csi.NodeGetInfoReque
 	if ns.config.manageNodeTopologyLabels {
 		// this will either overwrite or add the keys based on the driver name
 		segments[fmt.Sprintf(TopologyLabelNodePattern, driverName)] = ns.nodeID
-		segments[fmt.Sprintf(TopologyLabelTransportPattern, driverName)] = string(ns.getMounter().getTransport())
+		segments[fmt.Sprintf(TopologyLabelTransportPattern, driverName)] = string(ns.getMounter(ctx).getTransport()) // for backward compatibility, return the preferred transport
 		segments[fmt.Sprintf(TopologyLabelWekaLocalPattern, driverName)] = "true"
 	} else {
 		logger.Warn().Msg("Node topology labels management is disabled, using global label only")
