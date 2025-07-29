1370acf6369a377438eeb11fc28803c9a946c4a0
Sergey Berezansky <sergey@weka.io>
2025-07-29 11:46:19 +0300

    feat(CSI-381): make WEKA API timeout configurable

--- pkg/wekafs/volume_test.go
+++ pkg/wekafs/volume_test.go
@@ -13,12 +13,40 @@ func GetDriverForTest(t *testing.T) *WekaFsDriver {
 	ctx := context.Background()
 	nodeId := "localhost"
 	mutuallyExclusive := MutuallyExclusiveMountOptsStrings{"readcache,writecache,coherent,forcedirect", "sync,async", "ro,rw"}
-	driverConfig := NewDriverConfig("csi-volumes", "csi-vol-", "csi-snap-", "csi-seed-snap-",
-		true, true, true, true, true,
-		true, true, mutuallyExclusive,
-		1, 1, 1, 1, 1, 1, 1, 10,
-		true, true, true, "", "", "4.1", "v1", false, false, true,
-		"", false)
+	driverConfig := NewDriverConfig(
+		"csi-volumes",
+		"csi-vol-", "csi-snap-",
+		"csi-seed-snap-",
+		true,
+		true,
+		true,
+		true,
+		true,
+		true,
+		true,
+		mutuallyExclusive,
+		1,
+		1,
+		1,
+		1,
+		1,
+		1,
+		1,
+		10,
+		true,
+		true,
+		true,
+		"",
+		"",
+		"4.1",
+		"v1",
+		false,
+		false,
+		true,
+		"",
+		false,
+		120*time.Second,
+	)
 	driver, err := NewWekaFsDriver("csi.weka.io", nodeId, "unix://tmp/csi.sock", 10, "v1.0", CsiModeAll, false, driverConfig)
 	if err != nil {
 		t.Fatalf("Failed to create new driver: %v", err)
